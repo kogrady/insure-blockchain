fd927d14fede80e9b587a22b471ea8677747cdb3
Stephen McLean <smclean@guidewire.com>
2017-05-11 10:34:00 +0100

    Add nav bar routing

--- public/client/app/common/navbar/navbar.controller.js
+++ public/client/app/common/navbar/navbar.controller.js
@@ -1,7 +1,21 @@
 class NavbarController {
-  constructor() {
-    this.name = 'navbar';
+
+  constructor($state) {
+    this.$state = $state;
   }
+
+  /**
+   * Author: Stephan McLean
+   * Date: 11th May 2017
+   *
+   * Returns true if the current state matches the name passed in.
+   */
+  isActive(stateName) {
+    return this.$state.current.name === stateName;
+  }
+
 }
+
+NavbarController.$inject = ['$state'];
 
 export default NavbarController;
